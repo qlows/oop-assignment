4d48c0195b101a8c02a7ba316de17aff88e2dc65
Lemuel Javier <lemmytheleo@yahoo.com>
2021-11-25 19:51:19 -0500

    Added getters and toString() to classCustomer.cs
    getBookings() unfinished. Need to make Bookings class.

--- classCustomer.cs
+++ classCustomer.cs
@@ -8,6 +8,30 @@ class Customer
 
     public Customer(int id, string fName, string lName, int phone)
     {
-        
+        customerID = id;
+        this.fName = fName;
+        this.lName = lName;
+        phoneNumber = phone;
+        bookings = new Booking[numOfBookings];
+    }
+
+    public int getCustomerID() { return customerID; }
+    public string getFirstName() { return fName; }
+    public string getLastName() { return lName; }
+    public int getPhoneNumber() { return phoneNumber; }
+
+    public string getBookings() {
+        string s = "\nBooked Flights: ";
+        // Add for-loop to list booked flights when Bookings class is done.
+        return s;
+    }
+
+    public string toString()
+    {
+        string s = "Customer ID: " + customerID;
+        s += "\nFirst Name: " + fName;
+        s += "\nLast Name: " + lName;
+        s += "\nPhone Number: " + phoneNumber;
+        return s;
     }
 }
