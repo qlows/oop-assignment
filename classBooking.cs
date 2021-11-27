59b6177526e71ab88bac743e131e993c09191112
Lemuel Javier <lemmytheleo@yahoo.com>
2021-11-27 18:23:15 -0500

    Some edits.

--- classBooking.cs
+++ classBooking.cs
@@ -1,20 +1,28 @@
-class Booking
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace GroupProject
 {
-    private string date;
-    private int bookingID;
-    public Booking(string date, int bookingID)
+    class Booking
     {
-        Random Rand = new Random();
-        this.date = date;
-        bookingID = Rand.Next(10000, 99999);
-    }
-    public string getDate(String date){
-        return date;
-    }
-    public string toString() {
-        string s = "Booking";
-        s += "\nBooking id :" + bookingID;
-        s += "\nBooking date: " + date;;
-        return s;
+        private string date;
+        private int bookingID;
+        public Booking(string date, int bookingID)
+        {
+            Random Rand = new Random();
+            this.date = date;
+            bookingID = Rand.Next(10000, 99999);
+        }
+        public string getDate(String date){
+            return date;
+        }
+        public string toString() {
+            string s = "Booking";
+            s += "\nBooking id :" + bookingID;
+            s += "\nBooking date: " + date;;
+            return s;
+        }
     }
 }
--- classCustomer.cs
+++ classCustomer.cs
@@ -1,39 +1,46 @@
-class Customer
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace GroupProject
 {
-    private int customerID;
-    private string fName;
-    private string lName;
-    private int phoneNumber;
-    private Booking[] bookings;
-    private int numOfBookings;
-    
-
-    public Customer(int id, string fName, string lName, int phone)
+    class Customer
     {
-        customerID = id;
-        this.fName = fName;
-        this.lName = lName;
-        phoneNumber = phone;
-        bookings = new Booking[numOfBookings];
-    }
+        private int customerID;
+        private string fName;
+        private string lName;
+        private int phoneNumber;
+        private Booking[] bookings;
+        private int numOfBookings;
 
-    public int getCustomerID() { return customerID; }
-    public string getFirstName() { return fName; }
-    public string getLastName() { return lName; }
-    public int getPhoneNumber() { return phoneNumber; }
+        public Customer(int id, string fName, string lName, int phone)
+        {
+            customerID = id;
+            this.fName = fName;
+            this.lName = lName;
+            phoneNumber = phone;
+            bookings = new Booking[numOfBookings];
+        }
 
-    public string getBookings() {
-        string s = "\nBooked Flights: ";
-        // Add for-loop to list booked flights when Bookings class is done.
-        return s;
-    }
+        public int getCustomerID() { return customerID; }
+        public string getFirstName() { return fName; }
+        public string getLastName() { return lName; }
+        public int getPhoneNumber() { return phoneNumber; }
 
-    public string toString()
-    {
-        string s = "Customer ID: " + customerID;
-        s += "\nFirst Name: " + fName;
-        s += "\nLast Name: " + lName;
-        s += "\nPhone Number: " + phoneNumber;
-        return s;
+        public string getBookings() {
+            string s = "\nBooked Flights: ";
+            // Add for-loop to list booked flights when Bookings class is done.
+            return s;
+        }
+
+        public string toString()
+        {
+            string s = "Customer ID: " + customerID;
+            s += "\nFirst Name: " + fName;
+            s += "\nLast Name: " + lName;
+            s += "\nPhone Number: " + phoneNumber;
+            return s;
+        }
     }
 }
