4cd33a8b40969e7812fa6a06f5caf21be7009fc1
Lemuel Javier <54958092+GlitchAmbush@users.noreply.github.com>
2021-12-02 12:54:55 -0500

    Added Manager.cs and edited classCustomer.cs

---
+++ Manager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopAssignment
+{
+    class Manager
+    {
+        private oopAssignment.Customer[] clist;
+        private int numCustomer;
+        private int maxCustomer;
+
+        public Manager()
+        {
+            numCustomer = 0;
+            maxCustomer = 100;
+            clist = new Customer[maxCustomer];
+        }
+
+        public bool addCustomer(string fnam, string lName, int phone)
+        {
+            if (numCustomer < maxCustomer)
+            {
+                Random rnd = new Random();
+                int id = rnd.Next(10000, 99999);
+                clist[maxCustomer] = new Customer(id, fnam, lName, phone);
+                numCustomer++;
+                return true;
+            }
+            return false;
+        }
+
+        public string listAllCustomers()
+        {
+            string s = "=== Customer List ===\n";
+            for (int i = 0; i < numCustomer; i++)
+            {
+                if (clist[i].GetType() == typeof(Customer))
+                {
+                    s += ((Customer)clist[i]).ToString() + "\n";
+                }
+            }
+            return s;
+        }
+    }
+}
--- classCustomer.cs
+++ classCustomer.cs
@@ -11,8 +11,6 @@ namespace oopAssignment
         private string fName;
         private string lName;
         private int phoneNumber;
-        private Booking[] bookings;
-        private int numOfBookings;
 
         public Customer(int id, string fName, string lName, int phone)
         {
@@ -20,19 +18,12 @@ namespace oopAssignment
             this.fName = fName;
             this.lName = lName;
             phoneNumber = phone;
-            bookings = new Booking[numOfBookings];
         }
 
         public int getCustomerID() { return customerID; }
         public string getFirstName() { return fName; }
         public string getLastName() { return lName; }
         public int getPhoneNumber() { return phoneNumber; }
-
-        public string getBookings() {
-            string s = "\nBooked Flights: ";
-            // Add for-loop to list booked flights when Bookings class is done.
-            return s;
-        }
 
         public string toString()
         {
